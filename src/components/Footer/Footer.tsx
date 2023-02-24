5a01a333ff16c83b3a7971bb51adee76a1bb2e82
najmulhc <kridi32@outlook.com>
2023-02-24 21:52:59 +0600

    App Ready for shipping

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,10 @@
 const Footer: React.FC = () => {
-    return <div/>
+    return (
+      <footer className="card-container">
+        {" "}
+        <h3 className="footer-text">Copyright © 2023 Najmulhc</h3>{" "}
+      </footer>
+    );
 }
 
 export default Footer;
--- src/components/Home/HomeContainer.tsx
+++ src/components/Home/HomeContainer.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
+import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 const HomeContainer: React.FC = () => {
    return (
     <div> 
     <Navbar />
     <Outlet/>
+    <Footer/>
     </div>
    )
 }
